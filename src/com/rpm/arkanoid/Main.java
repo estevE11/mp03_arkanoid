86552f93043cdffd622655ab376790c322a49fb1
Roger Esteve <rogeresteve30@gmail.com>
2021-03-16 15:42:04 +0100

    base

--- src/com/rpm/arkanoid/Main.java
+++ src/com/rpm/arkanoid/Main.java
@@ -1,4 +1,43 @@
 package com.rpm.arkanoid;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+public class Main extends Canvas implements Runnable {
+    private Thread mainThread;
+
+    private boolean running = true;
+
+    public void run() {
+        while(this.running) {
+            this.update();
+            this.render();
+        }
+    }
+
+    public void update() {
+    }
+
+    public void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.black);
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+
+        g.dispose();
+        bs.show();
+    }
+
+    public void start() {
+        this.mainThread = new Thread(this, "GameThread");
+        this.mainThread.start();
+    }
 }
--- src/com/rpm/arkanoid/Start.java
+++ src/com/rpm/arkanoid/Start.java
@@ -1,8 +1,25 @@
 package com.rpm.arkanoid;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Start {
 
     public static void main(String args[]) {
-        System.out.println("Buenas");
+        Main main = new Main();
+        JFrame frame = new JFrame();
+
+        frame.setPreferredSize(new Dimension(640, 480));
+        frame.setMaximumSize(new Dimension(640, 480));
+        frame.setMinimumSize(new Dimension(640, 480));
+
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(main);
+
+        frame.setVisible(true);
+
+        main.start();
     }
 }
